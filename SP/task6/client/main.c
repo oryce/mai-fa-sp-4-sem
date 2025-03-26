74ab9da4880992f226c5063d98ca285e931ef67b
Sergey Mishin <serg@pochtus.ru>
2025-03-27 01:14:17 +0300

    Добавлена 6 задача

---
+++ SP/task6/client/main.c
@@ -0,0 +1,153 @@
+#define _GNU_SOURCE
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/** MAX_PATH isn't always available and is usually many times
+ smaller than this. */
+#define MAX_SENDABLE_PATH 16384
+
+static int cleanup(const int exit_code, const int socket_fd) {
+	close(socket_fd);
+	return exit_code;
+}
+
+static int cmd_loop(const int socket_fd) {
+	int exit_code = 0;
+
+	char* line_buf = NULL;
+	size_t line_buf_size = 0;
+
+	char* res_buf = NULL;
+	size_t res_buf_size = 0;
+
+	while (getline(&line_buf, &line_buf_size, stdin) > 0) {
+		{
+			char* newline = strrchr(line_buf, '\n');
+			if (newline) *newline = '\0';
+		}
+
+		const size_t line_size = strlen(line_buf) + 1;
+
+		// limit the path length to something sensible
+		if (line_size > MAX_SENDABLE_PATH) {
+			fprintf(stderr, "Warning: Your path is absurdly long, please correct it.");
+			continue;
+		}
+
+		// send buffer length
+		{
+			// XXX: cast size_t into something we can send over the network,
+			//      e.g. uint64_t.
+			const uint64_t send_size = htobe64(line_size);
+
+			unsigned char buffer[sizeof(send_size)];
+			memcpy(buffer, &send_size, sizeof(send_size));
+
+			if (send(socket_fd, buffer, sizeof(buffer), 0) != sizeof(buffer)) {
+				perror("send() failed");
+
+				exit_code = 5;
+				break;
+			}
+		}
+
+		// send the buffer
+		if (send(socket_fd, line_buf, line_size, 0) != line_size) {
+			perror("send() failed");
+
+			exit_code = 5;
+			break;
+		}
+
+		// read in the response
+		uint64_t res_len;
+
+		if (recv(socket_fd, &res_len, sizeof(res_len), MSG_WAITALL) != sizeof(res_len)) {
+			perror("recv() failed");
+
+			exit_code = 6;
+			break;
+		}
+
+		res_len = be64toh(res_len);
+
+		if (res_len > res_buf_size) {
+			char* new_res = (char*)realloc(res_buf, res_len);
+			if (new_res == NULL) {
+				fprintf(stderr, "failed to resize res buffer\n");
+
+				exit_code = 7;
+				break;
+			}
+			res_buf = new_res;
+			res_buf_size = res_len;
+		}
+
+		if (recv(socket_fd, res_buf, res_len, MSG_WAITALL) != res_len) {
+			perror("recv() failed");
+
+			exit_code = 6;
+			break;
+		}
+
+		printf("%s\n", res_buf);
+	}
+
+	free(line_buf);
+	free(res_buf);
+	return exit_code;
+}
+
+int main(const int argc, const char* const argv[]) {
+	if (argc != 3) {
+		printf("usage: %s <host> <port>\n", argv[0]);
+		return 0;
+	}
+
+	const char* host_in = argv[1];
+	const char* port_in = argv[2];
+
+	long port;
+	{
+		char* endp;
+
+		errno = 0;
+		port = strtol(port_in, &endp, 10);
+
+		if (*endp != '\0' || errno == ERANGE || port < 0 || port > 65535) {
+			fprintf(stderr, "Invalid port number: %s\n", port_in);
+			return 1;
+		}
+	}
+
+	in_addr_t host;
+	if (inet_pton(AF_INET, host_in, &host) != 1) {
+		perror("can't parse host address");
+		return 2;
+	}
+
+	int socket_fd;
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket() failed");
+		return 3;
+	}
+
+	struct sockaddr_in socket_addr;
+	socket_addr.sin_family = AF_INET;
+	socket_addr.sin_addr.s_addr = host;
+	socket_addr.sin_port = htons(port);
+
+	if (connect(socket_fd, (struct sockaddr*)&socket_addr, sizeof(socket_addr)) != 0) {
+		perror("connect() failed");
+		return 4;
+	}
+
+	const int exit_code = cmd_loop(socket_fd);
+
+	return cleanup(exit_code, socket_fd);
+}
---
+++ SP/task6/server/main.c
@@ -0,0 +1,232 @@
+#define _GNU_SOURCE
+
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#define CONNECTIONS_BACKLOG 5
+#define MAX_PATH 16384  // see client/main.c on path limits
+
+#define LOG(client_fd, message, ...)                                   \
+	do {                                                               \
+		printf("[client %d] " message "\n", client_fd, ##__VA_ARGS__); \
+	} while (0);
+
+static int cleanup(const int exit_code, const int server_fd) {
+	close(server_fd);
+	return exit_code;
+}
+
+static void send_string(const int client_fd, const char* const msg) {
+	LOG(client_fd, "send response: %s", msg);
+
+	const uint64_t msg_length = strlen(msg) + 1;
+	const uint64_t sent_length = htobe64(msg_length);
+
+	{
+		char buffer[sizeof(sent_length)];
+		memcpy(buffer, &sent_length, sizeof(sent_length));
+
+		if (write(client_fd, buffer, sizeof(buffer)) != sizeof(buffer)) {
+			LOG(client_fd, "cannot write response");
+			return;
+		}
+	}
+
+	if (write(client_fd, msg, msg_length) != msg_length) {
+		LOG(client_fd, "cannot write response");
+	}
+}
+
+static char* list_files(const char* const root_path) {
+	DIR* dir = opendir(root_path);
+	if (dir == NULL) return NULL;
+
+	char* res_buf = NULL;
+	size_t res_size = 0;
+
+	struct dirent* ent;
+	while ((ent = readdir(dir))) {
+		char abs_ent_path[PATH_MAX] = {'\0'};
+
+		if (*root_path == '/' && *(root_path + 1) == '\0') {
+			// Appending "root_path" adds another "/" here, which is unwanted.
+			snprintf(abs_ent_path, sizeof(abs_ent_path), "/%s", ent->d_name);
+		} else {
+			snprintf(abs_ent_path, sizeof(abs_ent_path), "%s/%s", root_path, ent->d_name);
+		}
+
+		const size_t abs_ent_size = strlen(abs_ent_path) + 1;
+		char* resized_buf = (char*)realloc(res_buf, res_size + abs_ent_size + 1);
+
+		if (resized_buf == NULL) {
+			closedir(dir);
+			free(res_buf);
+			return NULL;
+		}
+
+		if (res_buf == NULL) {
+			resized_buf[0] = '\0';
+		}
+
+		res_buf = resized_buf;
+		res_size += abs_ent_size + 1;
+
+		strncat(res_buf, abs_ent_path, abs_ent_size - 1);
+		strncat(res_buf, "\n", 1);
+	}
+
+	closedir(dir);
+	return res_buf;
+}
+
+static void process_client(const int client_fd) {
+	char* msg_buf = NULL;
+	size_t msg_buf_size = 0;
+	uint64_t msg_len;
+
+	while (recv(client_fd, &msg_len, sizeof(msg_len), MSG_WAITALL) == sizeof(msg_len)) {
+		if (msg_len == 0) {
+			continue;
+		}
+
+		msg_len = be64toh(msg_len);
+		LOG(client_fd, "read message of %zu bytes", msg_len);
+
+		// Resize the buffer to fit the new message.
+		if (msg_len > msg_buf_size) {
+			char* new_msg_buf = (char*)realloc(msg_buf, msg_len);
+			if (new_msg_buf == NULL) {
+				fprintf(stderr, "failed to resize msg buffer\n");
+				break;
+			}
+			msg_buf = new_msg_buf;
+			msg_buf_size = msg_len;
+		}
+
+		// Read the full message.
+		if (recv(client_fd, msg_buf, msg_len, MSG_WAITALL) != msg_len) {
+			LOG(client_fd, "failed to read message");
+			break;
+		}
+
+		LOG(client_fd, "read message: %s", msg_buf);
+
+		// Retrieve absolute path.
+		char resolved_path[MAX_PATH];
+		if (realpath(msg_buf, resolved_path) == NULL) {
+			send_string(client_fd, "no such path exists");
+			continue;
+		}
+
+		// Validate the path.
+		if (strcmp(msg_buf, resolved_path) != 0) {
+			send_string(client_fd, "not an absolute path");
+			continue;
+		}
+		{
+			struct stat path_stat;
+			stat(resolved_path, &path_stat);
+			if (!S_ISDIR(path_stat.st_mode)) {
+				send_string(client_fd, "not a directory");
+				continue;
+			}
+		}
+
+		char* listing = list_files(resolved_path);
+
+		if (listing == NULL) {
+			send_string(client_fd, "failed to list files");
+		} else {
+			send_string(client_fd, listing);
+			free(listing);
+		}
+	}
+
+	free(msg_buf);
+}
+
+int main(const int argc, const char* const argv[]) {
+	if (argc != 3) {
+		printf("usage: %s <host> <port>\n", argv[0]);
+		return 0;
+	}
+
+	const char* host_in = argv[1];
+	const char* port_in = argv[2];
+
+	long port;
+	{
+		char* endp;
+
+		errno = 0;
+		port = strtol(port_in, &endp, 10);
+
+		if (*endp != '\0' || errno == ERANGE || port < 0 || port > 65535) {
+			fprintf(stderr, "Invalid port number: %s\n", port_in);
+			return 1;
+		}
+	}
+
+	in_addr_t host;
+	if (inet_pton(AF_INET, host_in, &host) != 1) {
+		perror("can't parse host address");
+		return 6;
+	}
+
+	int server_fd;
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket() failed");
+		return 2;
+	}
+
+	{
+		int opt = 1;
+		if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) != 0) {
+			perror("setsockopt() failed");
+			return cleanup(7, server_fd);
+		}
+	}
+
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = host;
+	addr.sin_port = htons(port);
+
+	socklen_t addr_len = sizeof(addr);
+
+	if (bind(server_fd, (struct sockaddr*)&addr, addr_len) < 0) {
+		perror("bind() failed");
+		return cleanup(3, server_fd);
+	}
+
+	if (listen(server_fd, CONNECTIONS_BACKLOG) < 0) {
+		perror("listen() failed");
+		return cleanup(4, server_fd);
+	}
+
+	printf("Listening on port %ld\n", port);
+
+	while (1) {
+		int client_fd;
+		if ((client_fd = accept(server_fd, (struct sockaddr*)&addr, &addr_len)) < 0) {
+			if (errno == EINTR) {
+				// accept() interrupted
+				return cleanup(0, server_fd);
+			}
+			perror("accept() failed");
+			return cleanup(5, server_fd);
+		}
+
+		LOG(client_fd, "accepted connection");
+		process_client(client_fd);
+
+		LOG(client_fd, "connection ends");
+		close(client_fd);
+	}
+}
