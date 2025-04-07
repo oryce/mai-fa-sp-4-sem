ff9bde8d7d8ddb0536c52be9ab61b2dae5e0b2b0
alshved <159828879+alshved@users.noreply.github.com>
2025-04-07 23:32:11 +0300

    server-logger

--- logger/server_logger/src/server_logger.cpp
+++ logger/server_logger/src/server_logger.cpp
@@ -1,56 +1,82 @@
 #include <not_implemented.h>
-// #include <httplib.h>
+#include <httplib.h>
 #include "../include/server_logger.h"
+
+#include <server_logger_builder.h>
 
 #ifdef _WIN32
 #include <process.h>
-#elif
+#else
 #include <unistd.h>
 #endif
 
-server_logger::~server_logger() noexcept
-{
-    throw not_implemented("server_logger::~server_logger() noexcept", "your code should be here...");
+server_logger::~server_logger() noexcept {
+    httplib::Params par;
+    par.emplace("pid", std::to_string(server_logger::inner_getpid()));
+    _client.Get("/destroy", par, httplib::Headers());
 }
 
-logger& server_logger::log(
-    const std::string &text,
-    logger::severity severity) &
-{
-    throw not_implemented("const logger& server_logger::log(const std::string &, logger::severity) const &", "your code should be here...");
+logger &server_logger::log(const std::string &message,
+                           logger::severity severity) & {
+    std::stringstream stringstream;
+
+    stringstream << "[" << current_date_to_string() << " " << current_time_to_string() <<
+            "][" << severity_to_string(severity) << "] " << message;
+
+    httplib::Params par;
+    par.emplace("pid", std::to_string(server_logger::inner_getpid()));
+    par.emplace("sev", severity_to_string(severity));
+    par.emplace("message", stringstream.str());
+
+    _client.Get("/log", par, httplib::Headers());
+    return *this;
 }
 
-server_logger::server_logger(const std::string& dest,
-                             const std::unordered_map<logger::severity, std::pair<std::string, bool>> &streams)
-{
-    throw not_implemented("server_logger::server_logger(const std::string& ,const std::unordered_map<logger::severity, std::pair<std::string, bool>> &)", "your code should be here...");
+server_logger::server_logger(const std::string &dest,
+                             const std::unordered_map<logger::severity, std::pair<std::string, bool> > &
+                             streams) : _client(dest) {
+    for (const auto &[fst, snd]: streams) {
+        httplib::Params par;
+        par.emplace("pid", std::to_string(server_logger::inner_getpid()));
+        par.emplace("sev", severity_to_string(fst));
+        par.emplace("path", snd.first);
+        par.emplace("console", snd.second ? "1" : "0");
+
+        _client.Get("/init", par, httplib::Headers());
+    }
 }
 
-int server_logger::inner_getpid()
-{
+int server_logger::inner_getpid() {
 #ifdef _WIN32
-    return ::_getpid();
-#elif
+     return ::_getpid();
+#else
     return getpid();
 #endif
 }
 
-server_logger::server_logger(const server_logger &other)
-{
-    throw not_implemented("server_logger::server_logger(const server_logger &other)", "your code should be here...");
+server_logger::server_logger(const server_logger &other) : _client(
+    httplib::Client(other._client.host(), other._client.port())) {
 }
 
-server_logger &server_logger::operator=(const server_logger &other)
-{
-    throw not_implemented("server_logger &server_logger::operator=(const server_logger &other)", "your code should be here...");
+server_logger &server_logger::operator=(const server_logger &other) {
+    if (this != &other) {
+        httplib::Params par;
+        par.emplace("pid", std::to_string(server_logger::inner_getpid()));
+        _client.Get("/destroy", par, httplib::Headers());
+        _client = httplib::Client(other._client.host(), other._client.port());
+    }
+    return *this;
 }
 
-server_logger::server_logger(server_logger &&other) noexcept
-{
-    throw not_implemented("server_logger::server_logger(server_logger &&other) noexcept", "your code should be here...");
+server_logger::server_logger(server_logger &&other) noexcept : _client(std::move(other._client)) {
 }
 
-server_logger &server_logger::operator=(server_logger &&other) noexcept
-{
-    throw not_implemented("server_logger &server_logger::operator=(server_logger &&other) noexcept", "your code should be here...");
+server_logger &server_logger::operator=(server_logger &&other) noexcept {
+    if (this != &other) {
+        httplib::Params par;
+        par.emplace("pid", std::to_string(server_logger::inner_getpid()));
+        _client.Get("/destroy", par, httplib::Headers());
+        _client = std::move(other._client);
+    }
+    return *this;
 }
--- logger/server_logger/src/server_logger_builder.cpp
+++ logger/server_logger/src/server_logger_builder.cpp
@@ -1,42 +1,91 @@
 #include <not_implemented.h>
 #include "../include/server_logger_builder.h"
+#include <fstream>
 
-logger_builder& server_logger_builder::add_file_stream(
+logger_builder &server_logger_builder::add_file_stream(
     std::string const &stream_file_path,
-    logger::severity severity) &
-{
-    throw not_implemented("logger_builder& server_logger_builder::add_file_stream(std::string const &,logger::severity) &", "your code should be here...");
+    logger::severity severity) & {
+    auto it = _output_streams.find(severity);
+
+    if (it == _output_streams.end()) {
+        it = _output_streams.emplace(severity, std::make_pair(std::string(), false)).first;
+    }
+
+    it->second.first = stream_file_path;
+    return *this;
 }
 
-logger_builder& server_logger_builder::add_console_stream(
-    logger::severity severity) &
-{
-    throw not_implemented("logger_builder& server_logger_builder::add_console_stream(logger::severity) &", "your code should be here...");
+logger_builder &server_logger_builder::add_console_stream(
+    logger::severity severity) & {
+    auto it = _output_streams.find(severity);
+
+    if (it == _output_streams.end()) {
+        it = _output_streams.emplace(severity, std::make_pair(std::string(), false)).first;
+    }
+
+    it->second.second = true;
+    return *this;
 }
 
-logger_builder& server_logger_builder::transform_with_configuration(
+logger_builder &server_logger_builder::transform_with_configuration(
     std::string const &configuration_file_path,
-    std::string const &configuration_path) &
-{
-    throw not_implemented("logger_builder* server_logger_builder::transform_with_configuration(std::string const &configuration_file_path, std::string const &configuration_path)", "your code should be here...");
+    std::string const &configuration_path) & {
+    using namespace nlohmann;
+    std::ifstream config_stream(configuration_file_path);
+
+    if (!config_stream.is_open()) {
+        throw std::runtime_error("Failed to open config file: "
+                                 + configuration_file_path);
+    }
+
+    // Read config file
+    json config_json;
+    config_stream >> config_json;
+
+    // Get config for current logger
+    auto &config = config_json[configuration_path];
+
+    if (config.contains("format")) {
+        set_format(config["format"]);
+    }
+
+    for (auto &[key, value]: config.items()) {
+        if (key == "format") {
+            continue;
+        }
+        logger::severity severity = logger_builder::string_to_severity(key);
+        parse_severity(severity, value);
+    }
+    return *this;
 }
 
-logger_builder& server_logger_builder::clear() &
+void server_logger_builder::parse_severity(logger::severity sev, nlohmann::json& j)
 {
-    throw not_implemented("logger_builder& server_logger_builder::clear() &", "your code should be here...");
+    if (j.contains("console")) {
+        add_console_stream(sev);
+    }
+    if (j.contains("paths")) {
+        for (auto& path : j["paths"]) {
+            add_file_stream(path, sev);
+        }
+    }
 }
 
-logger *server_logger_builder::build() const
-{
-    throw not_implemented("logger *server_logger_builder::build() const", "your code should be here...");
+logger_builder &server_logger_builder::clear() & {
+    _output_streams.clear();
+    _destination = "http://127.0.0.1:9200";
+    return *this;
 }
 
-logger_builder& server_logger_builder::set_destination(const std::string& dest) &
-{
-    throw not_implemented("logger_builder& server_logger_builder::set_destination(const std::string&) &", "your code should be here...");
+logger *server_logger_builder::build() const {
+    return new server_logger(_destination, _output_streams);
 }
 
-logger_builder& server_logger_builder::set_format(const std::string &format) &
-{
-    throw not_implemented("logger_builder& server_logger_builder::set_format(const std::string &) &", "your code should be here...");
+logger_builder &server_logger_builder::set_destination(const std::string &dest) & {
+    _destination = dest;
+    return *this;
+}
+
+logger_builder &server_logger_builder::set_format(const std::string &format) & {
+    return *this;
 }
