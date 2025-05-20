2c9bf88a6db87c93e3df831ab21cade2a9c92068
kalane15 <kalane15@yandex.ru>
2025-05-20 21:19:06 +0300

    Update b_tree_disk_tests.cpp

--- associative_container/search_tree/indexing_tree/b_tree_disk/tests/b_tree_disk_tests.cpp
+++ associative_container/search_tree/indexing_tree/b_tree_disk/tests/b_tree_disk_tests.cpp
@@ -198,25 +198,40 @@ TEST(bTreeDiskPositiveTests, test4)
 {
     std::vector<test_data<SerializableInt, SerializableString>> expected_result =
             {
-                    test_data<SerializableInt, SerializableString>(1, 0, SerializableInt(1), SerializableString("a")),
-                    test_data<SerializableInt, SerializableString>(1, 1, SerializableInt(2), SerializableString("b")),
-                    test_data<SerializableInt, SerializableString>(1, 2, SerializableInt(3), SerializableString("d")),
-                    test_data<SerializableInt, SerializableString>(0, 0, SerializableInt(4), SerializableString("e")),
-                    test_data<SerializableInt, SerializableString>(1, 0, SerializableInt(15), SerializableString("c")),
-                    test_data<SerializableInt, SerializableString>(1, 1, SerializableInt(27), SerializableString("f"))
+                    test_data<SerializableInt, SerializableString>(1, 0, SerializableInt(2), SerializableString("b")),
+                    test_data<SerializableInt, SerializableString>(1, 1, SerializableInt(3), SerializableString("d")),
+                    test_data<SerializableInt, SerializableString>(1, 2, SerializableInt(4), SerializableString("e")),
+                    test_data<SerializableInt, SerializableString>(0, 0, SerializableInt(15), SerializableString("c")),
+                    test_data<SerializableInt, SerializableString>(1, 0, SerializableInt(45), SerializableString("k")),
+                    test_data<SerializableInt, SerializableString>(1, 1, SerializableInt(101), SerializableString("j")),
+                    test_data<SerializableInt, SerializableString>(1, 2, SerializableInt(456), SerializableString("h")),
+                    test_data<SerializableInt, SerializableString>(1, 3, SerializableInt(534), SerializableString("m"))
             };
     {
-        B_tree_disk<SerializableInt, SerializableString, std::less<>, 3> tree("temp_t.txt");
-        tree.emplace(1, std::string("a"));
-        tree.emplace(2, std::string("b"));
-        tree.emplace(15, std::string("c"));
-        tree.emplace(3, std::string("d"));
-        tree.emplace(4, std::string("e"));
-        tree.emplace(27, std::string("f"));
-        auto n1 = tree.disk_read(2);
-    }
+        B_tree_disk<SerializableInt, SerializableString, std::less<>, 4> tree("temp_t.txt");
+        tree.emplace(SerializableInt(1), SerializableString("a"));
+        tree.emplace(SerializableInt(2), SerializableString("b"));
+        tree.emplace(SerializableInt(15), SerializableString("c"));
+        tree.emplace(SerializableInt(3), SerializableString("d"));
+        tree.emplace(SerializableInt(4), SerializableString("e"));
+        tree.emplace(SerializableInt(100), SerializableString("f"));
+        tree.emplace(SerializableInt(24), SerializableString("g"));
+        tree.emplace(SerializableInt(456), SerializableString("h"));
+        tree.emplace(SerializableInt(101), SerializableString("j"));
+        tree.emplace(SerializableInt(45), SerializableString("k"));
+        tree.emplace(SerializableInt(193), SerializableString("l"));
+        tree.emplace(SerializableInt(534), SerializableString("m"));
 
-    B_tree_disk<SerializableInt, SerializableString, std::less<>, 3> tree2("temp_t.txt");
+
+    }
+    {
+        B_tree_disk<SerializableInt, SerializableString, std::less<>, 4> tree("temp_t.txt");
+        tree.erase(SerializableInt(1));
+        tree.erase(SerializableInt(100));
+        tree.erase(SerializableInt(193));
+        tree.erase(SerializableInt(24));
+    }
+    B_tree_disk<SerializableInt, SerializableString, std::less<>, 4> tree2("temp_t.txt");
     auto it = tree2.begin();
     auto end = tree2.end();
     while(it != end){
